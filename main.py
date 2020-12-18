8169f04a9a44b6a1c1db86a9b902b3bb3158b5e5
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2020-12-18 05:51:26 -0300

    fix: adjust shapes

--- main.py
+++ main.py
@@ -17,31 +17,34 @@ print(Y_treino.shape)
 print(X_teste.shape)
 print(Y_teste.shape)
 
+
 def raioGraficalizador(dados, intervalo, numeroIteracoes, taxaAprendizado):
-  fig,ax = plt.subplots()
-  x = [None] * dados.__len__()
+    fig, ax = plt.subplots()
+    x = [None] * dados.__len__()
 
-  for i in range(dados.__len__()):
-    x[i] = i*intervalo
+    for i in range(dados.__len__()):
+        x[i] = i*intervalo
 
-  ax.plot(x, dados)
-  plt.scatter(x, dados) # scatter são os quadradinhos na linha
+    ax.plot(x, dados)
+    plt.scatter(x, dados)  # scatter são os quadradinhos na linha
 
-  titulo = 'Taxa de aprendizado: {0} | Número de iterações: {1}'
-  titulo = titulo.format(taxaAprendizado, numeroIteracoes)
-  plt.title(titulo)
-  plt.xlabel('Número de iterações') #definindo nome do eixo X
-  plt.ylabel('Custo')
-  taxa = str(taxaAprendizado).replace('.','_')
-  nomeArquivo = '{0}and{1}.png'
-  nomeArquivo = nomeArquivo.format(numeroIteracoes, taxa)
-  plt.savefig(nomeArquivo)
-  plt.show()
+    titulo = 'Taxa de aprendizado: {0} | Número de iterações: {1}'
+    titulo = titulo.format(taxaAprendizado, numeroIteracoes)
+    plt.title(titulo)
+    plt.xlabel('Número de iterações')  # definindo nome do eixo X
+    plt.ylabel('Custo')
+    taxa = str(taxaAprendizado).replace('.', '_')
+    nomeArquivo = '{0}and{1}.png'
+    nomeArquivo = nomeArquivo.format(numeroIteracoes, taxa)
+    plt.savefig(nomeArquivo)
+    plt.show()
+
 
 vetor1 = [50, 100, 1000]
 vetor2 = [0.1, 0.01, 0.001]
 
 for i in vetor1:
-  for j in vetor2:
-    data = constroi_modelo(X_treino, Y_treino, X_teste, Y_teste, i, j, print_custo=False)
-    raioGraficalizador(data['custos'], 10, i, j)
+    for j in vetor2:
+        data = constroi_modelo(X_treino, Y_treino, X_teste,
+                               Y_teste, i, j, print_custo=False)
+        raioGraficalizador(data['custos'], 10, i, j)
--- treinamento_2nn.py
+++ treinamento_2nn.py
@@ -9,7 +9,7 @@ import scipy
 # Inicializar W[1], W[2], b[1] e b[2] aleatoriamente
 
 
-def inicializa_com_randoms(dim):
+def inicializa_com_randoms(dim, nr_neuronios):  # nr_neuronios da camada atual
     """
     Cria uma matriz w de dimensoes [dim][1], ou seja, um vetor em coluna, e o inicializa com zero. Inicializa b = 0.
 
@@ -22,13 +22,13 @@ def inicializa_com_randoms(dim):
     """
 
     # cria um vetor de determinada dimensao e seta suas posicoes valendo 0
-    w = np.random.rand(dim, 1)
-    b = np.random.rand()
+    w = np.random.rand(dim, nr_neuronios)
+    b = np.random.rand(nr_neuronios, 1)
 
     # garante que o vetor tenha sido criado com as dimensoes adequadas
-    assert(w.shape == (dim, 1))
+    assert(w.shape == (dim, nr_neuronios))
     # garante que b eh um float ou int
-    assert(isinstance(b, float) or isinstance(b, int))
+    assert(b.shape == (nr_neuronios, 1))
 
     return w, b
 
@@ -82,36 +82,74 @@ def treina(w, b, X, Y, num_iteracoes, taxa_aprendizado, print_custo=False):
     return parametros, gradientes, custos
 
 
+def sigmoide(z):
+    """
+    Computa a funcao logistica (sigmoide) de z
+
+    Parametros:
+    z -- Um escalar ou um vetor numpy de qualquer dimensão
+
+    Retorno:
+    s -- sigmoide(z)
+    """
+
+    s = 1 / (1 + np.exp(-z))  # np.exp(-z) implementa e^(-z)
+
+    return s
+
+
 # Enquanto (não convergir) ## Ou por um numero de épocas (no projeto atual vai ser 1000)
 
 def treina2nn(X, Y, m, alfa, num_iteracoes):
-    w, b = inicializa_com_randoms(4)
-    W1 = w
+    print("teste", X.shape[0])
+    w, b = inicializa_com_randoms(X.shape[0], 3)
+    W1 = w.T
     b1 = b
 
-    w, b = inicializa_com_randoms(3)
-    W2 = w
+    w, b = inicializa_com_randoms(3, 1)
+    W2 = w.T
     b2 = b
 
-# J - função de custo
-# dW[1] e db[1] - gradientes da camada oculta
-# dW[2] e db[2] - gradientes da camada de saída
+    # J - função de custo
+    # dW[1] e db[1] - gradientes da camada oculta
+    # dW[2] e db[2] - gradientes da camada de saída
     for i in range(num_iteracoes):
         J = 0
         dW1 = 0
         dW2 = 0
-        b1 = 0
-        b2 = 0
+        # b1 = 0
+        # b2 = 0
 
-    for i in range(m):  # Isso aqui ta iterando mas da pra vetorizar (da um puta ganho de desempenho)
+        # for i in range(m):  # Isso aqui ta iterando mas da pra vetorizar (da um puta ganho de desempenho)
+        Y_hat = sigmoide(np.dot(W1, X)+b1)
+        termo1 = Y*np.log(Y_hat)
+        termo2 = (1-Y)*np.log(1-Y_hat)
+
+        # custo = - 1/m * np.sum(termo1 + termo2)
+        # Z1 = (termo1 + termo2)
+
+        Z1 = np.dot(W1, X)+b1
+        A1 = sigmoide(Z1)  # Tanh() ou ReLU no lugar da sigmoide
         #     z[1](i) = W[1]x(i) + b[1]; a[1](i) = g(z[1](i)) ## Foward propagation [g(z[1](i)) - pode ser tanto tanh(z) quanto ReLU(z)]
+
+        Z2 = np.dot(W2, A1)+b2
+        A2 = sigmoide(Z2)
         #     z[2](i) = W[2]a[1](i) + b[2]; a[2](i) = σ(z[2](i)) ## Foward propagation [σ(z[2](i)) - Função sigmoide da camada de saída, ja foi implementado no outro projeto]
 
+        J1 = -1 * [Y*np.log(A2) + (1 - Y)*np.log(1 - A2)]
         #     J += − [y(i)log a[2](i) + (1 − y(i))log (1 − a[2](i))] ## Calcular o custo
 
+        # Y1 tem que ser o ultiimo indice de Y (debuga isso pra ver se o formato ta certo)
+        dZ2 = A2-Y1
         #     dz[2] = a[2](i) − y(1) ## Gradiente da camada de saída (camada externa)
+
+        dW2 += np.dot(dZ2, A1.T)
         #     dW[2] += dz[2]a[1](i)^T ## Agrega todas as instancias (camada externa)
+
+        dB2 += dZ2
         #     db[2] += dz[2] ## Agrega todas as instancias (camada externa)
+
+        # dZ1 = np.dot(W2, dZ2 ...
 
         #     dz[1] = W[2]^T dz[2] ∗ g[1]′(z[1]) ## Gradiente da camada de saída (camada oculta)
         #     dW[1] += dz[1] x(i)^T ## Agrega todas as instancias (camada oculta)
